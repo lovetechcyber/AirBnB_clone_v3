b158f5cbcd1982dd1eac67767e8c6ae32efc75eb
Lovetechcyber <lovetechcyb@gmail.com>
2024-05-01 15:18:05 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the FileStorage class
+Contains the FileStorage class.
 """
 
 import json
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
